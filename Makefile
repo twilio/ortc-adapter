1f07d29586e12e520bf516dfb92da6318d92395a
Mark Andrus Roberts <mroberts@twilio.com>
2015-09-08 18:41:38 -0700

    Add documentation and minification

--- Makefile
+++ Makefile
@@ -1,38 +1,83 @@
-BUILD=dist/ortc-adapter.js
+NAME=$(shell sed -n 's/^  "name": "\([^"]*\)\",$$/\1/p' package.json)
+VERSION=$(shell sed -n 's/^  "version": "\([^"]*\)\",$$/\1/p' package.json)
+MAIN=$(shell sed -n 's/^  "main": "\([^"]*\)\",$$/\1/p' package.json)
+
+DIST=dist
+
+JS=$(DIST)/$(NAME).js
+MIN_JS=$(JS:%.js=%.min.js)
+
+DOCS=$(DIST)/docs
 
 LIB=$(shell find lib -name \*.js)
-SRC=src/ortc-adapter.js
+SRC=src/$(NAME).js
+SRC_BUNDLE=$(SRC:%.js=%-bundle.js)
 
-BROWSERIFY=./node_modules/browserify/bin/cmd.js
-ESLINT=./node_modules/eslint/bin/eslint.js
-
-all: $(BUILD)
+all: .linted $(JS) $(MIN_JS) $(DOCS)
 
 clean:
-	rm -rf .lint dist
-
-# Lint
-
-.lint: $(LIB) $(SRC)
-	$(ESLINT) $?
-	touch .lint
-
-lint:
-	$(ESLINT) $(LIB) $(SRC)
+	rm -rf .linted $(DIST) $(BUNDLE)
 
 # Dependencies
 
-node_modules:
+BROWSERIFY=node_modules/browserify/bin/cmd.js
+ESLINT=node_modules/eslint/bin/eslint.js
+JSDOC=node_modules/jsdoc/jsdoc.js
+UGLIFYJS=node_modules/uglify-js/bin/uglifyjs
+
+$(BROWSERIFY):
 	npm install
 
-$(BROWSERIFY): node_modules
+$(ESLINT):
+	npm install
 
-$(ESLINT): node_modules
+$(JSDOC):
+	npm install
 
-# Build
+$(UGLIFYJS):
+	npm install
 
-$(BUILD): .lint $(BROWSERIFY) $(LIB) $(SRC)
-	mkdir -p dist
-	$(BROWSERIFY) $(SRC) -o $@
+# Lint
 
-.PHONY: all clean lint
+.linted: $(ESLINT) $(LIB) $(SRC)
+	$(ESLINT) $(filter-out node_modules%,$?)
+	touch .linted
+
+lint: $(ESLINT)
+	$(ESLINT) $(LIB) $(SRC)
+
+# Bundled JavaScript
+
+$(BUNDLE): .linted $(BROWSERIFY) $(LIB)
+	$(BROWSERIFY) -e $(MAIN) -o $@
+
+# JavaScript
+
+$(JS): $(BUNDLE) $(SRC)
+	mkdir -p $(DIST)
+	sed \
+    -e 's/$${name}/$(NAME)/' \
+    -e 's/$${version}/$(VERSION)/' \
+    $(SRC) \
+      | sed \
+        -e "/require('\.\/ortc-adapter-bundle\')/ {" \
+          -e 'r src/ortc-adapter-bundle.js' \
+          -e 'd' \
+        -e '}' \
+        >$@
+
+# Minified JavaScript
+
+$(MIN_JS): $(UGLIFYJS) $(JS)
+	$(UGLIFYJS) --comments '/^!/' <$(JS) >$@
+
+# Docs
+
+$(DOCS): $(JSDOC) $(LIB) $(SRC)
+	make docs
+
+docs: $(JSDOC)
+	rm -rf $(DOCS)
+	$(JSDOC) -r lib -d $(DOCS)
+
+.PHONY: all clean docs lint
--- src/ortc-adapter.js
+++ src/ortc-adapter.js
@@ -1,17 +1,73 @@
-'use strict';
-
-var ortcAdapter = require('../lib');
-
-(function(root, factory) {
+/*!
+ * ${name}.js ${version}
+ *
+ * Copyright (c) 2015 Twilio, Inc. All rights reserved. <https://twilio.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   - Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Twilio nor the names of its contributors may be used
+ *     to endorse or promote products derived from this software without
+ *     specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+/* eslint strict:0 */
+(function(root, selectWebRTC) {
+  var webrtc = selectWebRTC();
   /* global define:true */
   if (typeof define === 'function' && define.amd) {
-    define([], factory);
+    define([], webrtc);
   /* global module:true */
   } else if (typeof module === 'object' && module.exports) {
-    module.exports = factory();
+    module.exports = webrtc;
   } else {
-    root.ortcAdapter = factory();
+    for (var key in webrtc) {
+      root[key] = webrtc[key];
+    }
   }
 }(this, function() {
-  return ortcAdapter;
+  /* eslint max-len:0 */
+  var ortcAdapterBundle =
+    require('./ortc-adapter-bundle');
+  var ortcAdapter = ortcAdapterBundle(1);
+  var exports = {};
+  if (typeof window === 'undefined' || !window.navigator) {
+    console.log('This does not appear to be a browser; WebRTC APIs are unavailable.');
+  } else if (navigator.mozGetUserMedia && window.mozRTCPeerConnection) {
+    console.log('This appears to be Firefox; using moz-prefixed WebRTC APIs.');
+    /* globals mozRTCIceCandidate:true, mozRTCPeerConnection:true, mozRTCSessionDescription:true */
+    exports.RTCIceCandidate = window.RTCIceCandidate || mozRTCIceCandidate;
+    exports.RTCPeerConnection = mozRTCPeerConnection;
+    exports.RTCSessionDescription = window.RTCSessionDescription || mozRTCSessionDescription;
+  } else if (navigator.webkitGetUserMedia && window.chrome) {
+    console.log('This appears to be Chrome; using webkit-prefixed WebRTC APIs.');
+    /* globals RTCIceCandidate:true, webkitRTCPeerConnection:true, RTCSessionDescription:true */
+    exports.RTCIceCandidate = RTCIceCandidate;
+    exports.RTCPeerConnection = webkitRTCPeerConnection;
+    exports.RTCSessionDescription = RTCSessionDescription;
+  } else if (typeof RTCRtpSender) {
+    console.log('This appears to be an ORTC-compatible browser; using ORTC to WebRTC adapter.');
+    exports.RTCIceCandidate = ortcAdapter.RTCIceCandidate;
+    exports.RTCPeerConnection = ortcAdapter.RTCPeerConnection;
+    exports.RTCSessionDescription = ortcAdapter.RTCSessionDescription;
+  }
+  return exports;
 }));
